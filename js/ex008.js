2de4e84568797f4867fb9a5a4cee687e2ba29128
lucas <lucascostar098@gmail.com>
2022-01-17 09:33:10 -0300

    FIZ UMAS COISAS AI

---
+++ js/ex008.js
@@ -0,0 +1,6 @@
+var vel = 90.0
+console.log(`a velocidade do seu carro é ${vel}KM por hora`)
+    if (vel > 60) {        //condição simples
+        console.log('vc foi multado!')    
+}
+console.log('digija usando o sinto de segurança!')
