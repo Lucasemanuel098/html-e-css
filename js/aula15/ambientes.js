1ced6661b1332a6fc0f76b5b9a9e19bf7a52f038
lucas emanuel <96374851+Lucasemanuel098@users.noreply.github.com>
2022-03-22 11:32:46 -0300

    .

--- js/aula15/ambientes.js
+++ js/aula15/ambientes.js
@@ -1,9 +1,14 @@
-var num = [5, 8, 2, 9, 3]
+var num = [5, 8, 2, 9, 5, 8, 2, 9, 3]
 
-for (var pos = 0; pos >num.length; pos++) {
-    console.log (num)
+/*
+for (var pos = 0; pos < num.length; pos++) {
+    console.log (` A posição ${pos} tem o valor ${num[pos]}.`)
+}
+*/
+
+
+for (var pos = 0 in num) {
+    console.log (`A posição ${pos} tem o valor ${num[pos]}.`)
 }
 
-console.log (`O ventor tem ${num.length} posições`)
-
-console.log(num[3]) 
+console.log (num.indexOf(5))
